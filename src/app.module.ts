5e2db7e0969700767182ef1290717d80b17d49ec
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-07-13 17:32:19 +0300

    Create main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
 import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AuthModule } from './auth/auth.module';
       useFactory: getMongoConfig,
     }),
     AuthModule,
+    MainModule,
   ],
   controllers: [AppController],
   providers: [AppService],
