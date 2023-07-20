28dec43586c2c825b014c9a1f253c20498deeaed
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-07-20 18:35:05 +0300

    Upload files to local disc

--- src/mainPage/mainPage.controller.ts
+++ src/mainPage/mainPage.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { MainService } from './mainPage.service';
 import { MainDto } from './dto/mainPage.dto';
 import { MainModel } from './mainPage.model/mainPage.model';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileElementResponse } from './dto/file-element.response';
+import { MFile } from './dto/mfile.class';
 
 @Controller('main')
 export class MainController {
@@ -10,6 +21,25 @@ export class MainController {
   @Post('create')
   async create(@Body() dto: MainDto) {
     return this.mainService.create(dto);
+  }
+
+  @Post('upload')
+  @HttpCode(200)
+  @UseInterceptors(FileInterceptor('files'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<FileElementResponse[]> {
+    const saveArray: MFile[] = [file];
+    if (file.mimetype.includes('image')) {
+      const buffer = await this.mainService.convertToWebP(file.buffer);
+      saveArray.push(
+        new MFile({
+          originalname: `${file.originalname.split('.')[0]}.webp`,
+          buffer,
+        }),
+      );
+    }
+    return this.mainService.saveFiles(saveArray);
   }
 
   @Get('get')
--- src/mainPage/mainPage.service.ts
+++ src/mainPage/mainPage.service.ts
@@ -4,7 +4,13 @@ import { InjectModel } from 'nestjs-typegoose';
 import { MainModel } from './mainPage.model/mainPage.model';
 import { Model } from 'mongoose';
 import { MainDto } from './dto/mainPage.dto';
-import { getModelForClass } from '@typegoose/typegoose'; // Import from the correct path
+import { getModelForClass } from '@typegoose/typegoose';
+import { FileElementResponse } from './dto/file-element.response';
+import { format } from 'date-fns';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
+import * as sharp from 'sharp';
+import { MFile } from './dto/mfile.class';
 
 @Injectable()
 export class MainService {
@@ -16,6 +22,26 @@ export class MainService {
   async create(dto: MainDto): Promise<DocumentType<MainModel>> {
     const model = getModelForClass(MainModel);
     return model.findOneAndUpdate({}, dto, { upsert: true, new: true });
+  }
+
+  async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
+    const dateFolder = format(new Date(), 'yyyy-MM-dd');
+    const uploadFolder = `${path}/uploads/${dateFolder}`;
+    await ensureDir(uploadFolder);
+    const res: FileElementResponse[] = [];
+
+    for (const file of files) {
+      await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer);
+      res.push({
+        url: `${dateFolder}/${file.originalname}`,
+        name: file.originalname,
+      });
+      return res;
+    }
+  }
+
+  convertToWebP(file: Buffer): Promise<Buffer> {
+    return sharp(file).webp().toBuffer();
   }
 
   async getMainData(): Promise<MainModel> {
